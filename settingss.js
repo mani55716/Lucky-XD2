bdfe115b82e946401541d2c61e01adb4ac96f3b0
mani55716 <eventzoneff@gmail.com>
2025-09-22 18:56:23 +0545

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
-  OWNER_NUMBER: "", // put your phone number here
+  SESSION_ID: "malvin~Bjt1GASZ#itodgR2tPA6vuwvQRMV1WcE4CuZohZheNdsBRL2KhZA",  // add your Session Id here
+  OWNER_NUMBER: "9779807044421", // put your phone number here
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
 };
